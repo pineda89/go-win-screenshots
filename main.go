ea2b57be977ffe6db47bfa79910b2685a4b9b3be
pineda89 <danipinedasanchez@gmail.com>
2020-02-08 21:11:35 +0100

    Initial commit

---
+++ main.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"image"
+	"image/jpeg"
+	"io/ioutil"
+	"reflect"
+	"strconv"
+	"time"
+	"unsafe"
+)
+
+func main() {
+	// GetDC(0) is equals than GetDC(NULL). This will take screenshot of entire screen
+	screen_hdc = GetDC(0)
+	screen_horzres = GetDeviceCaps(screen_hdc, HORZRES)
+	screen_vertres = GetDeviceCaps(screen_hdc, VERTRES)
+	screen_rectangle = image.Rect(0, 0, screen_horzres, screen_vertres)
+	
+	img, _ := CaptureScreen()
+	myImg := image.Image(img)
+	outputfilename := strconv.FormatInt(time.Now().UnixNano(), 10) + ".jpg"
+	imgToFile(&myImg, outputfilename)
+	checkPixelColor(&myImg, 230, 480)
+}
+
+func checkPixelColor(i *image.Image, x int, y int) {
+	color := (*i).At(x, y)
+	r, g, b, _ := color.RGBA()
+	nr := r * 255 / 65535
+	ng := g * 255 / 65535
+	nb := b * 255 / 65535
+	fmt.Println(x, y, "color is", nr, ng, nb)
+}
+
+var (
+	screen_hdc       HDC
+	compatibleDC     HDC
+	screen_horzres   int
+	screen_vertres   int
+	screen_rectangle image.Rectangle
+)
+
+func CaptureScreen() (*image.RGBA, error) {
+	x, y := screen_rectangle.Dx(), screen_rectangle.Dy()
+
+	bt := BITMAPINFO{}
+	bt.BmiHeader.BiSize = uint32(reflect.TypeOf(bt.BmiHeader).Size())
+	bt.BmiHeader.BiWidth = int32(x)
+	bt.BmiHeader.BiHeight = int32(-y)
+	bt.BmiHeader.BiPlanes = 1
+	bt.BmiHeader.BiBitCount = 32
+	bt.BmiHeader.BiCompression = BI_RGB
+
+	ptr := unsafe.Pointer(uintptr(0))
+
+	compatibleDC = CreateCompatibleDC(screen_hdc)
+
+	DIBSection := CreateDIBSection(compatibleDC, &bt, DIB_RGB_COLORS, &ptr, 0, 0)
+	if DIBSection == 0 {
+		return nil, fmt.Errorf("Could not Create DIB Section err:%d.\n", GetLastError())
+	}
+	if DIBSection == InvalidParameter {
+		return nil, fmt.Errorf("CreateDIBSection invalid params\n")
+	}
+	defer DeleteObject(HGDIOBJ(DIBSection))
+
+	obj := SelectObject(compatibleDC, HGDIOBJ(DIBSection))
+	if obj == 0 {
+		return nil, fmt.Errorf("SelectObject err:%d.\n", GetLastError())
+	}
+	if obj == 0xffffffff { //GDI_ERROR
+		return nil, fmt.Errorf("GDI_ERROR err:%d.\n", GetLastError())
+	}
+	defer DeleteObject(obj)
+
+	BitBlt(compatibleDC, 0, 0, x, y, screen_hdc, screen_rectangle.Min.X, screen_rectangle.Min.Y, SRCCOPY)
+
+	DeleteDC(compatibleDC)
+
+	var slice []byte
+	hdrp := (*reflect.SliceHeader)(unsafe.Pointer(&slice))
+	hdrp.Data = uintptr(ptr)
+	hdrp.Len = x * y * 4
+	hdrp.Cap = x * y * 4
+
+	imageBytes := make([]byte, len(slice))
+
+	for i := 0; i < len(imageBytes); i += 4 {
+		imageBytes[i], imageBytes[i+2], imageBytes[i+1], imageBytes[i+3] = slice[i+2], slice[i], slice[i+1], slice[i+3]
+	}
+
+	img := &image.RGBA{imageBytes, 4 * x, image.Rect(0, 0, x, y)}
+	return img, nil
+}
+
+func imgToFile(img *image.Image, filename string) {
+	buf := new(bytes.Buffer)
+	jpeg.Encode(buf, *img, nil)
+	send_s3 := buf.Bytes()
+	ioutil.WriteFile(filename, send_s3, 0777)
+}
---
+++ win.go
@@ -0,0 +1,134 @@
+package main
+
+import (
+	"github.com/pkg/errors"
+	"syscall"
+	"unsafe"
+)
+
+var (
+	modgdi32               = syscall.NewLazyDLL("gdi32.dll")
+	moduser32              = syscall.NewLazyDLL("user32.dll")
+	modkernel32            = syscall.NewLazyDLL("kernel32.dll")
+	procGetDC              = moduser32.NewProc("GetDC")
+	procReleaseDC          = moduser32.NewProc("ReleaseDC")
+	procDeleteDC           = modgdi32.NewProc("DeleteDC")
+	procBitBlt             = modgdi32.NewProc("BitBlt")
+	procDeleteObject       = modgdi32.NewProc("DeleteObject")
+	procSelectObject       = modgdi32.NewProc("SelectObject")
+	procCreateDIBSection   = modgdi32.NewProc("CreateDIBSection")
+	procCreateCompatibleDC = modgdi32.NewProc("CreateCompatibleDC")
+	procGetDeviceCaps      = modgdi32.NewProc("GetDeviceCaps")
+	procGetLastError       = modkernel32.NewProc("GetLastError")
+)
+
+const (
+	BI_RGB           = 0
+	HORZRES          = 8
+	VERTRES          = 10
+	DIB_RGB_COLORS   = 0
+	InvalidParameter = 2
+	SRCCOPY          = 0x00CC0020
+)
+
+type (
+	HANDLE  uintptr
+	HWND    HANDLE
+	HGDIOBJ HANDLE
+	HDC     HANDLE
+	HBITMAP HANDLE
+)
+
+type BITMAPINFO struct {
+	BmiHeader BITMAPINFOHEADER
+	BmiColors *RGBQUAD
+}
+
+type BITMAPINFOHEADER struct {
+	BiSize          uint32
+	BiWidth         int32
+	BiHeight        int32
+	BiPlanes        uint16
+	BiBitCount      uint16
+	BiCompression   uint32
+	BiSizeImage     uint32
+	BiXPelsPerMeter int32
+	BiYPelsPerMeter int32
+	BiClrUsed       uint32
+	BiClrImportant  uint32
+}
+
+type RGBQUAD struct {
+	RgbBlue     byte
+	RgbGreen    byte
+	RgbRed      byte
+	RgbReserved byte
+}
+
+func GetDC(hwnd HWND) HDC {
+	ret, _, _ := procGetDC.Call(uintptr(hwnd))
+
+	return HDC(ret)
+}
+
+func GetDeviceCaps(hdc HDC, index int) int {
+	ret, _, _ := procGetDeviceCaps.Call(uintptr(hdc), uintptr(index))
+
+	return int(ret)
+}
+
+func ReleaseDC(hwnd HWND, hDC HDC) bool {
+	ret, _, _ := procReleaseDC.Call(uintptr(hwnd), uintptr(hDC))
+
+	return ret != 0
+}
+
+func DeleteDC(hdc HDC) bool {
+	ret, _, _ := procDeleteDC.Call(uintptr(hdc))
+	return ret != 0
+}
+
+func CreateCompatibleDC(hdc HDC) HDC {
+	ret, _, _ := procCreateCompatibleDC.Call(
+		uintptr(hdc))
+
+	return HDC(ret)
+}
+
+func SelectObject(hdc HDC, hgdiobj HGDIOBJ) HGDIOBJ {
+	ret, _, _ := procSelectObject.Call(uintptr(hdc), uintptr(hgdiobj))
+	return HGDIOBJ(ret)
+}
+
+func BitBlt(hdcDest HDC, nXDest, nYDest, nWidth, nHeight int, hdcSrc HDC, nXSrc, nYSrc int, dwRop uint) error {
+	ret, _, _ := procBitBlt.Call(uintptr(hdcDest), uintptr(nXDest), uintptr(nYDest), uintptr(nWidth), uintptr(nHeight), uintptr(hdcSrc), uintptr(nXSrc), uintptr(nYSrc), uintptr(dwRop))
+
+	if ret == 0 {
+		return errors.New("BitBlt error")
+	}
+
+	return nil
+}
+
+func GetLastError() uint32 {
+	ret, _, _ := procGetLastError.Call()
+	return uint32(ret)
+}
+
+func DeleteObject(hObject HGDIOBJ) bool {
+	ret, _, _ := procDeleteObject.Call(uintptr(hObject))
+
+	return ret != 0
+}
+
+func CreateDIBSection(hdc HDC, pbmi *BITMAPINFO, iUsage uint, ppvBits *unsafe.Pointer, hSection HANDLE, dwOffset uint) HBITMAP {
+	ret, _, _ := procCreateDIBSection.Call(
+		uintptr(hdc),
+		uintptr(unsafe.Pointer(pbmi)),
+		uintptr(iUsage),
+		uintptr(unsafe.Pointer(ppvBits)),
+		uintptr(hSection),
+		uintptr(dwOffset))
+
+	return HBITMAP(ret)
+}
